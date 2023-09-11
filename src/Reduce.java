370da588fc031f1fef9bf02b79817b6d7eba371a
Eric Liang <eliangca@outlook.com>
2023-09-11 13:44:11 -0400

    Added Reduce.java add checked boxes

---
+++ src/Reduce.java
@@ -0,0 +1,18 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int steps = 0;
+
+        while (i != 0) {
+            if (i % 2 == 0) {
+                i = Math.floorDiv(i, 2);
+            }
+            else {
+                i -= 1;
+            }
+            steps += 1;
+        }
+
+        System.out.println(steps);
+    }
+}
