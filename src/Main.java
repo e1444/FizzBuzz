cfdcb799ac5820a559dd1e60fc5a6ff03a8b46e1
Eric Liang <eliangca@outlook.com>
2023-09-11 13:33:00 -0400

    Added todo items

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+
+        // soln taken from
+        // https://stackoverflow.com/questions/2550310/can-a-main-method-of-class-be-invoked-from-another-class-in-java
+        FizzBuzz.main(null);
+    }
+}
